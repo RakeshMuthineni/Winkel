eddd64b6d9b9d948d8afd53acd950d4cb35bdcb1
RakeshMuthineni <rakeshmuthineni9@gmail.com>
2021-11-24 14:50:26 +0530

    changed coading

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['winkel-course-Kart.eba-kssquwtj.us-west-2.elasticbeanstalk.com
-']
+ALLOWED_HOSTS = []
 
 
 # Application definition
